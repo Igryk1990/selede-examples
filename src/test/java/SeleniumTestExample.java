9498bdbf3a5583db7568469330d5885221a1b47d
mustang90 <mustang90@bk.ru>
2021-05-14 10:51:52 +0300

    add simple test

--- src/test/java/SeleniumTestExample.java
+++ src/test/java/SeleniumTestExample.java
@@ -1,18 +1,29 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class SeleniumTestExample {
 
     private WebDriver driver;
 
     @BeforeMethod
-    public void setupTest() {
-        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
-        driver = new ChromeDriver();
+    public void setupTest() throws MalformedURLException {
+//        System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+//        driver = new ChromeDriver();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capabilities);
         driver.manage().window().maximize();
     }
 
